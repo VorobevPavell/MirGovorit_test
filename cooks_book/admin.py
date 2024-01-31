5168b49b51da62dec063ad0b440a71feebcb6361
vorobev pasha <vorobevpasha@MacBook-Pro-vorobev.local>
2024-01-31 10:51:26 +0300

    Добавил админку

--- cooks_book/admin.py
+++ cooks_book/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Products, RecipeProduct
 
+
+admin.site.register(Recipe)
+admin.site.register(Products)
+admin.site.register(RecipeProduct)
